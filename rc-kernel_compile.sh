49906d3648c9c067382894e69a0faf26375d50b8
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2017-11-03 18:59:32 +0530

    kernel compile automated
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

---
+++ rc-kernel_compile.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+major_version=
+minor_version=
+patch_version=
+echo "Lets compile the new kernel ...but before that do a flight check.."
+echo
+
+echo "Get the kernel from kernel.org"
+
+echo
+echo " This is for rc kernel.."
+wget -c https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-$major_version.$minor_version-$patch_version.tar.gz 
+
+echo $?
+
+echo 
+echo "Get the sign for the kernel ..."
+echo
+
+wget -c  https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-$major_version.$minor_version-$patch_version.tar.sign
+
+echo $?
+
+echo 
+echo " Move the kernel to /usr/src dir "
+echo
+
+mv -v linux-$major_version.$minor_version-$patch_version.tar.gz /usr/src/
+mv -v linux-$major_version.$minor_version-$patch_version.tar.sign /usr/src/
+
+echo
+
+echo 
+echo " Getting into the dir ..."
+
+cd /usr/src
+
+pwd
+
+
+echo 
+echo " Decompress the downloaded kernel ..."
+echo
+
+tar -xvzf linux-$major_version.$minor_version-$patch_version.tar.gz
+
+echo $?
+
+
+echo 
+echo " Lets check the kernel signing..."
+echo
+
+gpg2 --verify linux-$major_version.$minor_version-$patch_version.tar.sign
+
+
+
+echo 
+echo " Untar the kernel ..."
+echo 
+
+tar -xvf linux-$major_version.$minor_version-$patch_version.tar
+
+echo
+echo " Get into the kernel tree and clean it .."
+echo
+
+cd linux-$major_version.$minor_version-$patch_version
+
+make clean && make mrproper
+echo
+echo "Check the lastest kernel dir in /usr/src direcoty"
+echo
+
+ls -al /usr/src
+
+
+echo " Now get running kernel conf and build .config "
+echo
+
+zcat /proc/config.gz > .config
+
+echo "Check the timestamp on the file for the sanity"
+echo
+
+ls -al .config
+
+echo " Lets do the config ...run "make olddefconfig""
+echo
+
+make olddefconfig
+
+echo
+echo " Then make it ..."
+
+time make -j9
+
+echo $?
+
+if [ $? == 0 ]
+then
+
+echo "Done"
+
+else 
+
+echo "Error encountered"
+
+fi
+
+echo " Installing the modules ..."
+echo
+
+make modules_install
+
+echo 
+echo " Copying the build kernel to boot directory"
+echo
+
+cp arch/x86/boot/bzImage /boot/vmlinuz-$major_version.$minor_version-$patch_version
+
+echo
+echo " Cross check the item ..."
+echo
+
+ls -al /boot/vmlinuz-*
+
+echo
+echo " Copy the System.map file to /boot dir"
+echo
+
+cp System.map /boot/System.map-$major_version.$minor_version-$patch_version
+
+echo
+echo " Again checking the timestamp"
+
+ls -al /boot/System.map-*
+
+echo
+echo " Copying the .config file to /boot dir "
+echo
+cp .config /boot/config-$major_version.$minor_version-$patch_version
+
+echo 
+echo "Backup the old System.map file ..."
+echo
+
+cd /boot
+pwd
+
+
+mv  Systeme.map-$major_version.$minor_version-$patch_version System.map
+
+echo 
+#echo " Unlink the kernel pointer and link to latest kernel "
+#echo
+
+#unlink kernel 
+
+#ln -s vmlinux-`date +"%Y%m%d_%H%M%S"` vmlinuz
+
+#echo
+
+echo " Now,you should fix the EFI boot loader with correct entry and reboot"
