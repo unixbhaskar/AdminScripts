7f17cc9a1251d8b8636912b920a47bab4b5dc959
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2018-03-13 09:09:08 +0530

    modified for generability
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- awsconsole_stat_fetch.sh
+++ awsconsole_stat_fetch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ###############################################################################################################################
 # This script is to fetch the instance id , corresponding EIP (Elastic IP) and the current status of the instance.The data    #
@@ -19,16 +19,16 @@ printf "\n Collecting instance details.. $now, Please wait ***\n"
 
 ## APAC Region Keys
 
-AP_AWS_ACCESS_KEY_ID=access_key
-AP_AWS_SECRET_KEY_ID=secret_key
+AP_AWS_ACCESS_KEY_ID=
+AP_AWS_SECRET_KEY_ID=
 AP_REGION_NAME=ap-southeast-1
 EC2_URL=https://ap-southeast-1.ec2.amazonaws.com
 
 
 ### EU Region Keys
 
-EU_AWS_ACCESS_KEY_ID=access_key
-EU_AWS_SECRET_KEY_ID=secret_key
+EU_AWS_ACCESS_KEY_ID=
+EU_AWS_SECRET_KEY_ID=
 EU_REGION_NAME=eu-west-1
 EC2_URL=https://eu-west-1.ec2.amazonaws.com
 
@@ -36,8 +36,8 @@ EC2_URL=https://eu-west-1.ec2.amazonaws.com
 
 ### US Region  Keys
 
-US_AWS_ACCESS_KEY_ID=access_key
-US_AWS_SECRET_KEY_ID=secret_key
+US_AWS_ACCESS_KEY_ID=
+US_AWS_SECRET_KEY_ID=
 US_REGION_NAME=us-east-1
 EC2_URL=https://us-east-1.ec2.amazonaws.com
 
@@ -46,14 +46,14 @@ EC2_URL=https://us-east-1.ec2.amazonaws.com
 ## $AP_AWS_ACCESS_KEY => holds the APAC Region Access key and $AP_AWS_SECRET_KEY=> holds the APAC region secret key 
 ## fetches the instance id, Elastic IP address and the Current status of the instance
 
-ec2din  --aws-access-key $AP_AWS_ACCESS_KEY_ID  -W $AP_AWS_SECRET_KEY_ID --url $EC2_URL --region $AP_REGION_NAME | grep 'INSTANCE' |  grep -v 'stopped' | cut  -f 2,7,6,14,15,17 | gawk -F=  ' { print $1 $2 $3 $4 $5 $6 }' > /home/$USER/AP_INSTANCE_DETAILS.txt
+ec2din  --aws-access-key $AP_AWS_ACCESS_KEY_ID  -W $AP_AWS_SECRET_KEY_ID --url $EC2_URL --region $AP_REGION_NAME | grep 'INSTANCE' |  grep -v 'stopped' | cut  -f 2,7,6,14,15,17 | gawk -F=  ' { print $1 $2 $3 $4 $5 $6 }' > $HOME/AP_INSTANCE_DETAILS.txt
 
 
 ## Fetch details for the EU region
-ec2din  --aws-access-key $EU_AWS_ACCESS_KEY_ID  -W $EU_AWS_SECRET_KEY_ID --url $EC2_URL --region $EU_REGION_NAME | grep 'INSTANCE' | grep -v 'stopped' | cut  -f 2,7,15,6,14,15,17 | gawk -F=  ' { print $1 $2 $3 $4 $5 $6 }' > /home/$USER/EU_INSTANCE_DETAILS.txt
+ec2din  --aws-access-key $EU_AWS_ACCESS_KEY_ID  -W $EU_AWS_SECRET_KEY_ID --url $EC2_URL --region $EU_REGION_NAME | grep 'INSTANCE' | grep -v 'stopped' | cut  -f 2,7,15,6,14,15,17 | gawk -F=  ' { print $1 $2 $3 $4 $5 $6 }' > $HOME/EU_INSTANCE_DETAILS.txt
 
 ## Fetch details for the US Region
-ec2din  --aws-access-key $US_AWS_ACCESS_KEY_ID  -W $US_AWS_SECRET_KEY_ID --url $EC2_URL --region $US_REGION_NAME  | grep 'INSTANCE' | grep -v 'stopped' | cut  -f 2,7,15,6,14,15,17 | gawk -F= ' { print $1 $2 $3 $4 $5 $6 }' > /home/$USER/US_INSTANCE_DETAILS.txt
+ec2din  --aws-access-key $US_AWS_ACCESS_KEY_ID  -W $US_AWS_SECRET_KEY_ID --url $EC2_URL --region $US_REGION_NAME  | grep 'INSTANCE' | grep -v 'stopped' | cut  -f 2,7,15,6,14,15,17 | gawk -F= ' { print $1 $2 $3 $4 $5 $6 }' > $HOME/US_INSTANCE_DETAILS.txt
 
 
 printf "\n\n\n **Completed....\n"
--- site_fetch.sh
+++ site_fetch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ###############################################################################################################################
 # This script is to fetch the instance id , corresponding EIP (Elastic IP) and the current status of the instance.The data    #
@@ -19,16 +19,16 @@ printf "\n Collecting websites details.. $now, Please wait.......... \n"
 
 ## APAC Region Keys
 
-AP_AWS_ACCESS_KEY_ID=access_key
-AP_AWS_SECRET_KEY_ID=secret_key
+AP_AWS_ACCESS_KEY_ID=
+AP_AWS_SECRET_KEY_ID=
 AP_REGION_NAME=ap-southeast-1
 EC2_URL=https://ap-southeast-1.ec2.amazonaws.com
 
 
 ### EU Region Keys
 
-EU_AWS_ACCESS_KEY_ID=access_key
-EU_AWS_SECRET_KEY_ID=secret_key
+EU_AWS_ACCESS_KEY_ID=
+EU_AWS_SECRET_KEY_ID=
 EU_REGION_NAME=eu-west-1
 EC2_URL=https://eu-west-1.ec2.amazonaws.com
 
@@ -36,29 +36,28 @@ EC2_URL=https://eu-west-1.ec2.amazonaws.com
 
 ### US Region  Keys
 
-US_AWS_ACCESS_KEY_ID=access_key
-US_AWS_SECRET_KEY_ID=secret_key
+US_AWS_ACCESS_KEY_ID=
+US_AWS_SECRET_KEY_ID=
 US_REGION_NAME=us-east-1
 EC2_URL=https://us-east-1.ec2.amazonaws.com
 
 
 
 #Fetch details for the APAC region
-ec2din  --aws-access-key $AP_AWS_ACCESS_KEY_ID  -W $AP_AWS_SECRET_KEY_ID --url $EC2_URL --region $AP_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Sites*" |  gawk  '{ print  $5, $14 }'  > /home/$USER/AP_SITE_DETAILS.txt
+ec2din  --aws-access-key $AP_AWS_ACCESS_KEY_ID  -W $AP_AWS_SECRET_KEY_ID --url $EC2_URL --region $AP_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Sites*" |  gawk  '{ print  $5, $14 }'  > $HOME/AP_SITE_DETAILS.txt
 
 
 
 ## Fetch details for the EU region
-ec2din  --aws-access-key $EU_AWS_ACCESS_KEY_ID  -W $EU_AWS_SECRET_KEY_ID --url $EC2_URL --region $EU_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Site*" |  gawk  '{ print  $5, $14 }'  > /home/$USER/EU_SITE_DETAILS.txt
+ec2din  --aws-access-key $EU_AWS_ACCESS_KEY_ID  -W $EU_AWS_SECRET_KEY_ID --url $EC2_URL --region $EU_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Site*" |  gawk  '{ print  $5, $14 }'  > $HOME/EU_SITE_DETAILS.txt
 
 
 #Fetch details for the US region
-ec2din  --aws-access-key $US_AWS_ACCESS_KEY_ID  -W $US_AWS_SECRET_KEY_ID --url $EC2_URL --region $US_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Site*" |  gawk  '{ print   $5, $14 }'  > /home/$USER/US_SITE_DETAILS.txt
+ec2din  --aws-access-key $US_AWS_ACCESS_KEY_ID  -W $US_AWS_SECRET_KEY_ID --url $EC2_URL --region $US_REGION_NAME |grep  -e "INSTANCE" -e "AWS-Site*" |  gawk  '{ print   $5, $14 }'  > $HOME/US_SITE_DETAILS.txt
 
-echo
-echo
-echo
-#echo " Formating the files....please wait...."
+
+
+printf " Formating the files....please wait....\n"
 
 #source format.sh
 
