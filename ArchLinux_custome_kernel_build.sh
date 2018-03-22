59504d88b612acb0e072edd5bb79fb1cbb7f5168
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2018-03-22 16:40:56 +0530

    modified and better less interactive
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- ArchLinux_custome_kernel_build.sh
+++ ArchLinux_custome_kernel_build.sh
@@ -3,8 +3,8 @@
 #This script's information based on this wiki page : https://wiki.archlinux.org/index.php/Kernels/Arch_Build_System
 
 EFIBOOTDIR=/boot/efi/EFI/ArchLinux
-DT=`date '+%d_%m_%Y_%T'`
-
+DT=`date '+%d%m%Y'`
+EFIBOOTENTRY=/boot/efi/loader/entries/
 
 printf "This script is running to autome the custom/latest kernel build process...have patience \n\n\n"
 
@@ -21,34 +21,36 @@ printf "Create a directory to hold and download the latest kernel from kernel.or
 
 mkdir -p build_cust_kernel_$DT
 
-printf "Get into it...\n\n"
+printf "Get into it...\n\n\n"
 
 cd build_cust_kernel_$DT
 pwd
 
-printf "Checking out latest linux from kernel.org \n\n\n"
+printf "Checking out latest linux from kernel.org \n\n\n\n"
 
 asp checkout linux
 
-if [[ $! == 0 ]];then
- 
-  echo "Alright.. continue..."
-
+if [[ $? == 0 ]];then
+   echo "Alright.. continue..."
 else
-  
   echo "Nope abort!"
    exit 1
 fi
 
-printf "Change the kernel name in PKGBUILD file to the pkgbase parameter ....\n\n\n"
+printf "\n\n\n\n\n"
 
-cd /build_cust_kernel_$DT/linux/repos/core-x86_64/
+printf "*********Configuring PKGBUILD********** \n\n\n\n"
+
+printf "Change the kernel name in PKGBUILD file to the pkgbase parameter ....\n\n\n\n"
+
+cd linux/repos/core-x86_64/
 
 
-sed -i 's/pkgbase=linux/#pkgbase-linux/' PKGBUILD
+sed -i 's/pkgbase=linux/#pkgbase=linux/' PKGBUILD
 
 
-sed -i "s/#pkgbase=linux-custom/pkgbase=linux-$(echo $DT)/"  PKGBUILD
+sed -i "/pkgbase=linux-custom/s/^#/pkgbase=linux-$(echo $kernel) /"  PKGBUILD
+
 
 
 patch_version=`grep "pkgver=" PKGBUILD`
@@ -67,18 +69,33 @@ printf "Lets do the compiling now ....\n\n\n"
 time makepkg -s
 
 
-printf "Install the package with pacman ..\n\n\n"
+printf "Install the generated headers,kernel and doc packages with pacman ..\n\n\n"
 
-pacman -U linux-$DT-headers-*.tar.xz
+sudo pacman -U --noconfirm linux-$kernel-headers-1-x86_64.pkg.tar.xz
 
-pacman -U linux-$DT-*.tar.xz
+sudo pacman -U  --noconfirm linux-$kernel-1-x86_64.pkg.tar.xz 
+
+sudo pacman -U  --noconfirm linux-$kernel-docs-1-x86_64.pkg.tar.xz 
 
 
 printf "Done..now copy over the image to EFI dir..\n\n\n\n"
 
-cp -v /boot/vmlinuz-linux-$DT $EFIBOOTDIR
-cp -v /boot/initramfs-linux-$DT.img $EFIBOOTDIR
+sudo cp -v /boot/vmlinuz-linux-$kernel $EFIBOOTDIR
+sudo cp -v /boot/initramfs-linux-$kernel.img $EFIBOOTDIR
 
+
+printf "Fixed the boot entry now...\n\n\n\n"
+
+
+echo "title ArchLinux" > $EFIBOOTENTRY/ArchLinux.conf 
+echo "linux /EFI/ArchLinux/vmlinuz-linux-$kernel" >> $EFIBOOTENTRY/ArchLinux.conf
+echo "initrd /EFI/ArchLinux/initramfs-linux-$kernel.img" >> $EFIBOOTENTRY/ArchLinux.conf
+echo "options root=PARTUUID=e79c9191-3302-4dc8-a62a-5cc22d266643  loglevel=3  systemd.show_status=true rw" >> $EFIBOOTENTRY/ArchLinux.conf 
+
+
+printf "..Okay let us see how it looks ...\n\n\n\n"
+
+cat $EFIBOOTENTRY/ArchLinux.conf
 
 exit 0
 
