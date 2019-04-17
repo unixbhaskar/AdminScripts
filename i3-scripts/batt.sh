3466ec20d18d7016b5d815323423307f5862f9ae
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2019-04-17 16:23:23 +0530

    modified two scripts battery and i3blocks conf
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- i3-scripts/batt.sh
+++ i3-scripts/batt.sh
@@ -19,22 +19,25 @@ stat=0
 # Exit if no battery was found
 if [[ "$(acpi)" == "" ]]; then exit 0; fi
 
-state=$(acpi | sed -n 's/Battery [0-9]: \([A-Z]\).*, .*/\1/p')
+state=$(acpi | sed -n 's/Battery [0]: \([A-Z]\).*, .*/\1/p')
+state1=$(acpi | sed -n 's/Battery [1]: \([A-Z]\).*, .*/\1/p')
 chg=$(acpi | sed -n 's/Battery [0]:.*, \([0-9]\{1,3\}\)%.*/\1/p')
 chg2=$(acpi | sed -n 's/Battery [1]:.*, \([0-9]\{1,3\}\)%.*/\1/p')
 
 # Charging or Unknown
-if [[ $state = "C" ]] || [[ $state = "U" ]]; then
+if [[ $state == "C" ]] || [[ $state == "U" ]] && [[ $state1 == "C" ]] || [[ $state1 == "U" ]];then
+        
 	icon=""
-	stat=35
-else
-	if [[ $chg -le 10 ]] || [[ $chg2 -le 10 ]];  then
+fi
+if [[ $state != "C" ]] && [[ $state1 != "C" ]];then
+           icon=""
+fi
+    
+if [[ $chg -le 10 ]] || [[ $chg2 -le 10 ]];then 
 		icon=""
 		stat=33
-	else
-		icon=""
-	fi
-fi
+fi          	
+
 
 full="$icon $chg% $icon $chg2%"
 short="$chg% $chg2%"
