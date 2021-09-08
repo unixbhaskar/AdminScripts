47c88eb35476d07fc32053b08be8ae5e7248ed84
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-09-08 14:38:22 +0530

    Added capability show extra information by clicking on the module

--- i3-scripts/mem_use.sh
+++ i3-scripts/mem_use.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+case $BLOCK_BUTTON in
+	1) notify-send "Memory hog processes:
+	$(ps axch -o cmd:15,%mem --sort=-%mem | head)";;
+esac
 mem=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
-
-echo "Mem:$mem"
+echo -e "<span color='black' bgcolor='#01A252'> : $mem</span>\n"
+#echo "Mem:$mem"
