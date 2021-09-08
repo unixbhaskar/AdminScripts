1044fdd92d6b9d211a98eade705a166e2b9d4883
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-09-08 14:37:08 +0530

    Added some cosmetic flavor by adding background color

--- i3-scripts/diskio.sh
+++ i3-scripts/diskio.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash  
+#!/usr/bin/env bash
 #===============================================================================
 #
 #          FILE: diskio.sh
-# 
-#         USAGE: ./diskio.sh 
-# 
-#   DESCRIPTION: 
-# 
+#
+#         USAGE: ./diskio.sh
+#
+#   DESCRIPTION:
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: sysstat package
 #          BUGS: ---
@@ -26,7 +26,8 @@ show=$(iostat -p sda | grep $disk  | gawk '{ print "Disk:"$1 ",Read:"$6 ",Write:
 #show1=$(iostat -p sda | grep $root1 | gawk '{ print "Disk:"$1 ",Read:"$5 ",Write:" $6}')
 
 #if [[ $root != "" ]];then
-echo $show
+echo -e "<span color='black' bgcolor='#008B8B'>:$show</span>\n"
+#echo $show
 #fi
 
 #if [[ $root1 != "" ]];then
