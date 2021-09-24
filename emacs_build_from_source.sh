8c2a1b1ed375fbfa2974c04e9e4b440b496276a6
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-09-25 02:42:20 +0530

    Emacs build from the source
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

---
+++ emacs_build_from_source.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: emacs_build_from_source.sh
+#
+#         USAGE: ./emacs_build_from_source.sh
+#
+#   DESCRIPTION: Build Emacs from source
+#
+#       OPTIONS:
+#  REQUIREMENTS:
+#          BUGS: ---
+#         NOTES: Clone this repo git://git.savannah.gnu.oit
+#        AUTHOR: Bhaskar Chowdhury (https://about.me/unixbhaskar), unixbhaskar@gmail.com
+#  ORGANIZATION: Independent
+#       CREATED: 09/24/2021 17:37
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+basepath=/home/bhaskar/git-linux/
+
+cd $basepath
+
+if [ ! -d emacs ];then
+	git clone git@git://git.savannah.gnu.git
+	cd emacs
+
+elif [ -d emacs ];then
+       cd emacs
+       git pull
+
+else
+	printf "The Vim directory already exists....not cloning \n"
+	exit 1
+fi
+
+printf "Lets build it on.....$(hostname)"
+
+
+./configure
+
+make
+
+
+if [ $? -eq 0 ] ; then
+
+	printf "Do you want to install system wide? [Y/N]: %s"
+	read -r res
+
+	if [ "$res" == "Y" ];then
+		sudo make install
+	else
+		printf "Okay, you not want to install it system wide....not installing "
+fi
+fi
