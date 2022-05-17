8f04b5a9892eff8d88c8248d91392301c775b760
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2022-05-18 04:47:03 +0530

    Arch Linux Custom kernel build script
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- ArchLinux_custom_kernel_build.sh
+++ ArchLinux_custom_kernel_build.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+#Author : Bhaskar CHowdhury
+#LICENSE : GPL-2
 #This script information based on this wiki page : https://wiki.archlinux.org/index.php/Kernels/Arch_Build_System
 
-NOCOLOR="\033[0m"
 EFIBOOTDIR=/boot/efi/EFI/ArchLinux
-DT=`date '+%d%m%Y'`
+DT=$(date '+%d%m%Y')
 EFIBOOTENTRY=/boot/efi/loader/entries
-source /home/bhaskar/colors.sh
-build_dir=/home/bhaskar/latest_kernel_`hostname`_$DT
+build_dir=/home/bhaskar/latest_kernel_$(hostname)_$DT
 TM="/usr/bin/time -f"
-
-printf "${Bright}${Red}This script is running to autome the custom/latest kernel build process...have patience${NOCOLOR} \n\n\n"
+KERNEL_PKG_DIR="/home/bhaskar/latest_kernel_build/linux/repos/core-x86_64/"
 
 printf "Hostname: %s\nDate    : %s\nUptime  :%s\n\n"  "$(hostname -s)" "$(date)" "$(uptime)"
 
 
-printf "Get the latest kernel version from ${Blue}kernel.org \n\n\n"
+kernel=$(arch_linux_latest_tag | sed -e 's/...$//' | tr -d "-" | tr -d "arch" | tr -d "v")
+printf '%s\n' "$kernel"
 
-kernel=`curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)'| grep 4.16`
-printf "${Bright}${GREEN}$kernel${NOCOLOR} \n"
+# checking if the build is exist , if not ceate it
 
-printf "Create a directory to hold and download the latest kernel from ${Blue}kernel.org${NOCOLOR} \n\n\n"
-
-if [[ ! -d $build_dir ]];then
-   printf "${Bright}${Green}Created it ${NOCOLOR} \n\n"
+if [[ ! -d "$build_dir" ]];then
+   printf "Created it\n\n"
     mkdir -p $build_dir
 else
-   printf "${Bright}${LimeYellow}Already exists! ${NOCOLOR} \n\n"
+   printf "Already exists! \n\n"
 fi
 
 printf "\n\n Get into it...\n\n\n"
@@ -34,83 +31,92 @@ printf "\n\n Get into it...\n\n\n"
 cd $build_dir
 pwd
 
-printf "Checking out latest linux from ${Blue}kernel.org${NOCOLOR} \n\n\n\n"
+printf "Checking out latest linux \n\n\n\n"
 
 asp checkout linux
 
 if [[ $? == 0 ]];then
-   printf "${GREEN}Alright.. continue...${NOCOLOR}\n\n\n"
+   printf "Alright.. continue...\n\n\n"
 else
-  printf "${RED}Nope abort!${NOCOLOR}\n\n\n"
+  printf "Nope abort!\n\n\n"
    exit 1
 fi
 
-printf "*********${Bright}${Cyan}Configuring PKGBUILD${NOCOLOR}********** \n\n\n\n"
+printf "*********Configuring PKGBUILD********** \n\n\n\n"
 
-printf "${LimeYellow}Customizing few varibles in the PKGBUILD file...${NOCOLOR}\n\n\n\n"
-
-cd linux/repos/core-x86_64/
+arch_repo=linux/repos/core-x86_64/
+cd $arch_repo
 
 sudo zcat /proc/config.gz > config
 
-sed -i 's/pkgbase=linux/#pkgbase=linux/' PKGBUILD
+# Turning off like this help build kernel faster, but pose difficulty while debugging
 
+sed -i "s/"CONFIG_DEBUG_KERNEL=y"/"CONFIG_DEBUG_KERNEL=n"/" config
+grep DEBUG_KERNEL config
+sed -i "s/"CONFIG_DEBUG_INFO=y"/"CONFIG_DEBUG_INFO=n"/" config
+grep DEBUG_INFO config
 
-sed -i "/pkgbase=linux-custom/s/^#/pkgbase=linux-$(echo $kernel) /"  PKGBUILD
+sed -i "s/pkgbase=linux/pkgbase=$(hostname)-$(echo $kernel) /"  PKGBUILD
+pkgver=$(grep "pkgver" PKGBUILD | head -1)
+sed -i "s/$(echo $pkgver)/pkgver=$(echo $kernel) /" PKGBUILD
 
+sed -i '7d' PKGBUILD
+sed -i '7i _srcver=${pkgver%%%.*}-arch1 '  PKGBUILD
+sed -i '17d' PKGBUILD
+sed -i '17i _srcname=${pkgver%%%.*}-arch1'  PKGBUILD
 
-cn=`echo $kernel | cut -d"." -f1-2`
-
-sed -i "s/_srcname=linux-4.15/_srcname=linux-$(echo $cn) /" PKGBUILD
-
-patch_version=`grep "pkgver=" PKGBUILD`
-
-sed -i "s/$(echo $patch_version)/pkgver=$(echo $kernel)/" PKGBUILD
-
+sed -i '19d' PKGBUILD
+sed -i '19i \"$_srcname::https://github.com/archlinux/linux/archive/refs/tags/v$_srcname.tar.gz\"' PKGBUILD
+sed -i '31d' PKGBUILD
+sed -i '31i export KBUILD_BUILD_HOST=Bhaskar_ThinkPad_x250' PKGBUILD
+sed -i '32d' PKGBUILD
+sed -i '32i export KBUILD_BUILD_USER=Bhaskar' PKGBUILD
+sed -i '36d' PKGBUILD
+sed -i '36i cd src/linux-$_srcname' PKGBUILD
 sed -i 's/#make oldconfig/make olddefconfig/' PKGBUILD
+sed -i '62d' PKGBUILD
+sed -i '62i make V=1 ARCH=x86_64 -j4' PKGBUILD
+sed -i '63d' PKGBUILD
+sed -i '174,192 s/^/#/' PKGBUILD
+sed -i '193d' PKGBUILD
+sed -i '193i pkgname=("$pkgbase" "$pkgbase-headers")' PKGBUILD
 
-sed -i '171d;172d' PKGBUILD
+printf "As we have change the PKGBUILD file ,we need to generate the new CHECKSUM the file ....  \n\n\n"
 
-printf "As we have change the PKGBUILD file ,we need to generate the new ${Magenta}CHECKSUM the file .... ${NOCOLOR} \n\n\n"
-
-#makepkg -g 
+#makepkg -g
 
 updpkgsums
 
 
-printf "\n\n\n Lets do the ${Bright}${Green}compiling now ${NOCOLOR} ....\n\n\n"
+printf "\n\n\n Lets do the compiling now  ....\n\n\n"
 
 $TM "\t\n\n Elapsed Time : %E \n\n"  makepkg -s
 
 
-printf "Install the generated ${PowderBlue}headers,${PowderBlue}kernel and ${PowderBlue}doc packages with pacman .. ${NOCOLOR} \n\n\n"
-
-sudo pacman -U --noconfirm linux-$kernel-headers-$kernel-1-x86_64.pkg.tar.xz
-
-sudo pacman -U  --noconfirm  linux-$kernel-$kernel-1-x86_64.pkg.tar.xz 
-
-sudo pacman -U  --noconfirm linux-$kernel-docs-$kernel-1-x86_64.pkg.tar.xz 
+printf "Install the generated headers,kernel and doc packages with pacman .. \n\n\n"
 
 
-printf "\n\n\n Done..now copy over the image to ${Yellow}EFI dir..${NOCOLOR} \n\n\n\n"
+cd $KERNEL_PKG_DIR
 
-sudo cp -v /boot/vmlinuz-linux-$kernel $EFIBOOTDIR
-sudo cp -v /boot/initramfs-linux-$kernel.img $EFIBOOTDIR
+sudo pacman -U  --noconfirm $(hostname)-$kernel-$kernel-1-x86_64.pkg.tar.zst
 
-
-printf "${Bright}${Blue}Fixed the boot entry now ${NOCOLOR}...\n\n\n\n"
+sudo pacman -U --noconfirm $(hostname)-$kernel-headers-$kernel-1-x86_64.pkg.tar.zst
 
 
-sudo echo "title ArchLinux" > $EFIBOOTENTRY/ArchLinux.conf 
-sudo echo "linux /EFI/ArchLinux/vmlinuz-linux-$kernel" >> $EFIBOOTENTRY/ArchLinux.conf
-sudo echo "initrd /EFI/ArchLinux/initramfs-linux-$kernel.img" >> $EFIBOOTENTRY/ArchLinux.conf
-sudo echo "options root=PARTUUID=e79c9191-3302-4dc8-a62a-5cc22d266643  loglevel=3  systemd.show_status=true rw" >> $EFIBOOTENTRY/ArchLinux.conf 
+# printf "\n\n\n Done..now copy over the image to ${Yellow}EFI dir..${NOCOLOR} \n\n\n\n"
+
+# sudo cp -v /boot/vmlinuz-$(hostname)-$kernel $EFIBOOTDIR/$(hostname)/
+# sudo cp -v /boot/initramfs-$(hostname)-$kernel.img $EFIBOOTDIR/$(hostname)/
 
 
-printf "${Bright}${Cyan} Okay let us see how it looks ${NOCOLOR} ...\n\n\n\n"
+# printf "Fixed the boot entry now ...\n\n\n\n"
 
-sudo cat $EFIBOOTENTRY/ArchLinux.conf
+# echo "title ArchLinux" | sudo tee  $EFIBOOTENTRY/ArchLinux.conf
+# echo "linux /EFI/ArchLinux/vmlinuz-$(hostname)-$kernel" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
+# echo "initrd /EFI/ArchLinux/initramfs-$(hostname)-$kernel.img" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
+# echo "options root=PARTUUID=9e3d2f9a-4846-3049-97fc-b5e5c61820ae  loglevel=3  systemd.show_status=true rw" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
 
+# printf "\n\n\n Modified the UEFI script...  \n\n"
+
+# echo "\EFI\ArchLinux\vmlinuz-$(hostname)-$kernel root=PARTUUID=9e3d2f9a-4846-3049-97fc-b5e5c61820ae  loglevel=3  systemd.show_status=true rw initrd=\EFI\ArchLinux\initramfs-$(hostname)-$kernel.img" | sudo tee  /boot/efi/EFI/archlinux.nsh
 exit 0
-
-
