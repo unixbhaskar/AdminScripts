1350bc2ea32e407e2d701bcd80b9275be189e499
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2022-08-27 18:16:03 +0530

    Modified heavily for need
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- software_install.sh
+++ software_install.sh
@@ -5,7 +5,8 @@
 #
 #         USAGE: ./software_install_.sh
 #
-#   DESCRIPTION:
+#   DESCRIPTION: This script will fetch and build software on freshly install
+#                system
 #
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
@@ -18,62 +19,46 @@
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
-OS=$(uname -n)
-pkgs="i3 i3block i3lock vim scrot vimb zathura screen git neofetch newsboat
-calcurse htop lsof feh st dmenu mutt postfix lynx w3m pass  gpg2 curl wget
-syslog-ng iptraf-ng moreutils findutils dhcp dhcpcd wpa_supplicant sudo vifm
-surf mpv ffmpeg isync cmus cronie imgmagick zip xz psutils xclip xsel xdotool
-clipmenu clipmenud telegram tcpdump ipcalc sysstats etckeeper aspell emacs"
 
-source /home/bhaskar/colors.sh
+pkgs="i3 i3block i3lock vim scrot zathura screen git newsboat
+calcurse htop lsof feh  mutt postfix lynx w3m pass gpg2 curl wget
+syslog-ng iptraf-ng moreutils findutils dhcp dhcpcd wpa_supplicant sudo
+mpv ffmpeg isync cmus cronie imgmagick zip xz psutils xclip xsel xdotool
+clipmenu clipmenud tcpdump ipcalc sysstats etckeeper aspell emacs"
+
 
 if [[ $UID != 0 ]];then
-    echo ${Bright}${Red}"You have to be superuser to run this script.${Normal}"
+    echo "You have to be superuser to run this script."
     exit 1
 fi
 
-printf "\n\n\t Installing minimal environment for the productivity on ${Bright}${Cyan}$(echo $OS)${Normal} \n\n"
+printf "\n\n\t Installing minimal environment for the productivity on $hostname.... \n\n"
 
 software_install() {
-	printf "Which distro [G/D/S/O/A] : %s"
+	printf "Which distro [G/D/S/A] : %s"
 	read -r distro
 	if [[ $distro == "G" ]];then
-		gentoo_soft_install
+		install_on_gento
 		pull_down_my_settings_from_github
 	elif [[ $distro  == "D" ]];then
-	        debian_soft_install
+	        install_on_debian
 		pull_down_my_settings_from_github
 	elif [[ $distro == "S" ]];then
-		slackware_soft_install
-		pull_down_my_settings_from_github
-	elif [[ $distro == "O" ]];then
-		opensuse_soft_install
+		install_on_slackware
 		pull_down_my_settings_from_github
 	elif [[ $distro == "A" ]];then
-		arch_soft_install
+		install_on_arch
 		pull_down_my_settings_from_github
 	else
 		echo Nothing Chosen..aborting!
 		exit 1
 	fi
 }
-# declare -A osInfo;
-# osInfo[/etc/arch-release]="pacman -S --yes"
-# osInfo[/etc/gentoo-release]="emerge -vt"
-# osInfo[/etc/SuSE-release]="zypper --yes install"
-# osInfo[/etc/debian_version]="apt-get --yes install"
-# osInfo[/etc/slackware_version]="slackpkg --yes install"
-# for f in ${!osInfo[@]}
-# do
-#     if [[ -f $f ]];then
-#         echo Package manager: ${osInfo[$f]}
-#     fi
-# done
 
-
+# Dotfiles
 pull_down_my_settings_from_github() {
 
-printf "Getting the ${Bright}${Blue}dotfiles${Normal} from ${Bright}${Magenta}GitHub${Normal}....\n"
+printf "Getting the dotfiles from GitHub....\n"
 
 git clone --recurse-submodules --remote-submodules git@github.com:unixbhaskar/dotfiles.git
 cd dotfiles
@@ -81,47 +66,64 @@ cp -v * $HOME/
 
 }
 
-gentoo_soft_install() {
+# Fetch st from my github repo
+
+fetch_st() {
+  git clone git@github.com:unixbhaskar/st_terminal_build
+  cp st_terminal_build/st_sol $HOME/bin/
+  cp st_terminal_build/st_gruvbox $HOME/bin/
+}
+
+# Fetch vimb from github
+
+fetch_vimb() {
+  git clone git@github.com:unixbhaskar/vimb_build
+  cp vimb_build/vimb $HOME/bin/
+}
+
+# Fetch dmenu from github
+
+fetch_dmenu() {
+  git clone git@github.com:unixbhaskar/dmenu_build
+  cp dmenu_build/dmenu $HOME/bin/
+  cp dmenu_build/dmenu_run $HOME/bin/
+  cp dmenu_build/dmenu_path $HOME/bin/
+}
+
+install_on_gentoo() {
 
 if [[ $(command -v emerge) != "" ]];then
 	emerge -vt ${pkgs}
 else
-	echo ${Bright}${Red}This is not a Gentoo system ${Normal}
+	echo This is not a Gentoo system
 	exit 1
 fi
 }
 
-debian_soft_install() {
+install_on_debian() {
 if [[ $(command -v apt-get) != "" ]];then
       apt-get install ${pkgs} --yes
 else
-	echo ${Bright}${Red}This is not a Debian system ${Normal}
+	echo This is not a Debian system
 	exit 1
 fi
 }
 
-slackware_soft_install() {
+install_on_slackware() {
 if [[ $(command -v slackpkg) != "" ]];then
 	slackpkg install ${pkgs} --yes
 else
-	echo ${Bright}${Red}This is not a Slackware system ${Normal}
+	echo This is not a Slackware system
 	exit 1
 fi
 }
-opensuse_soft_install() {
-if [[ $(command -v zypper) != "" ]];then
-        zypper in ${pkgs} --yes
-else
-	echo ${Bright}${Red}This is not a Opensuse system ${Normal}
-	exit 1
-fi
 
-}
-arch_soft_install() {
+
+install_on_arch() {
 if [[ $(command -v pacman) != "" ]];then
 	pacman -S ${pkgs} --noconfirm
 else
-	echo ${Bright}${Red}This is not a Arch linux system ${Normal}
+	echo This is not a Arch linux system
 	exit 1
 fi
 }
