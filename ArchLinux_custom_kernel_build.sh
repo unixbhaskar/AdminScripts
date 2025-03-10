dde9c4aa50aa1b89c0ae3575670dd1e00e6a4ab9
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2025-03-11 03:40:00 +0530

    Arch Linux custom kernel build from upstream linux kernel source
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- ArchLinux_custom_kernel_build.sh
+++ ArchLinux_custom_kernel_build.sh
@@ -1,122 +1,133 @@
 #!/usr/bin/env bash
-
-#Author : Bhaskar CHowdhury
-#LICENSE : GPL-2
-#This script information based on this wiki page : https://wiki.archlinux.org/index.php/Kernels/Arch_Build_System
-
-EFIBOOTDIR=/boot/efi/EFI/ArchLinux
+get_make=$(command -v make)
+get_elapsed_time="/usr/bin/time -f"
+untar_it="tar -xJvf"
+get_it=$(command -v secure_kernel_tarball)
+NOTIFY=$(command -v notify-send)
+NOCOLOR="\033[0m"
 DT=$(date '+%d%m%Y')
-EFIBOOTENTRY=/boot/efi/loader/entries
-build_dir=/home/bhaskar/latest_kernel_$(hostname)_$DT
-TM="/usr/bin/time -f"
-KERNEL_PKG_DIR="/home/bhaskar/latest_kernel_build/linux/repos/core-x86_64/"
+build_dir="/home/bhaskar/custom_kernel_$(hostname)_$DT"
 
-printf "Hostname: %s\nDate    : %s\nUptime  :%s\n\n"  "$(hostname -s)" "$(date)" "$(uptime)"
-
-
-kernel=$(arch_linux_latest_tag | sed -e 's/...$//' | tr -d "-" | tr -d "arch" | tr -d "v")
-printf '%s\n' "$kernel"
-
-# checking if the build is exist , if not ceate it
-
-if [[ ! -d "$build_dir" ]];then
-   printf "Created it\n\n"
-    mkdir -p $build_dir
-else
-   printf "Already exists! \n\n"
+if test ! -d "$build_dir";then
+	mkdir -p "$build_dir"
 fi
 
-printf "\n\n Get into it...\n\n\n"
+cd "$build_dir" || exit 1
 
-cd $build_dir
-pwd
 
-printf "Checking out latest linux \n\n\n\n"
+#vim "/etc/mkinitcpio.d/linux-upstream.preset"
 
-asp checkout linux
+archlinux_kernel_build() {
 
-if [[ $? == 0 ]];then
-   printf "Alright.. continue...\n\n\n"
+which_kernel
+
+printf "\n\n"
+
+# Change the kernel name
+sh -c "sudo sed -i '4d' /etc/mkinitcpio.d/linux-upstream.preset"
+sh -c "sudo sed -i '4i ALL_kver=\"/boot/vmlinuz-linux-upstream-${kernel}-Archlinux\"' /etc/mkinitcpio.d/linux-upstream.preset"
+
+sh -c "sudo sed -i '8d' /etc/mkinitcpio.d/linux-upstream.preset"
+sh -c "sudo sed -i '8i default_image=\"/boot/initramfs-linux-upstream.img-${kernel}-Archlinux\"' /etc/mkinitcpio.d/linux-upstream.preset"
+
+eval ${get_it} ${kernel}
+
+
+#Untar it
+$untar_it linux-$kernel.tar.xz
+
+
+#Get into the kernel direcory
+cd linux-$kernel
+
+# Take away the debug package option for faster build
+
+sed -i '8d' scripts/package/PKGBUILD
+sed -i '8i  _extrapackages=${PACMAN_EXTRAPACKAGES-headers api-headers}' scripts/package/PKGBUILD
+sed -i '114,125 s/^/#/' scripts/package/PKGBUILD
+
+# Creating a new checksum, because we have modified the PKGBUILD file
+
+#updpkgsums scripts/package/PKGBUILD
+
+#Clean the dir
+$get_make clean && $get_make mrproper
+
+#Copying existing/running kernel config
+zcat /proc/config.gz >.config
+
+#Disable this option to shorten the compile time
+scripts/config --disable DEBUG_KERNEL
+grep DEBUG_KERNEL .config
+
+#Disable this option to shorten the compile time
+scripts/config --disable DEBUG_INFO
+grep DEBUG_INFO .config
+
+# Enable USB storage module for external HDD
+scripts/config --enable USB_STORAGE
+
+# Set the local version of the operating system name
+scripts/config --set-str LOCALVERSION "-Archlinux"
+
+#Make sure the flags symbols are set correctly with an updated value
+#$get_make  ARCH=x86_64 olddefconfig
+yes '' | make localmodconfig
+
+
+# Now build it
+$get_elapsed_time "\n\n\tTime Elapsed: %E\n\n" $get_make ARCH=x86_64 V=1 -j$(getconf _NPROCESSORS_ONLN) pacman-pkg
+
+if test $? -eq 0
+then
+     $NOTIFY --urgency=critical "Kernel building done"
+
+
+     printf "\nFirst take backup of the existing kernel and initramf to the backups.....\n\n"
+
+     sudo sh -c "mv -v "/boot/*-Archlinux" "/home/bhaskar/arch_linux_old_kernels/""
+
+     printf "\n Cross checking the latest movement of the kernel and initramfs in backup\n\n"
+
+     find  "/home/bhaskar/arch_linux_old_kernels/" -type f  -ls
+
+     printf "\n Check the latest genrated package files.....\n"
+
+     find . -maxdepth 1 -name "*.zst" -type f -ls
+
+     # printf "\n Signing the packages with my GPG key.....\n"
+
+     # sh -c "gpg2 --detach-sign --pinentry-mode loopback --passphrase --passphrase-fd 0 --output *.pkg.tar.zst.sig --sign *.pkg.tar.zst"
+
+     printf "\n Now install the generated headers,kernel and doc packages with pacman .. \n\n\n"
+
+
+     sudo pacman -U  --noconfirm linux-upstream-$kernel_Archlinux-1-x86_64.pkg.tar.zst
+
+     sudo pacman -U --noconfirm linux-upstream-headers-$kernel_Archinux-1-x86_64.pkg.tar.zst
+
+     sudo pacman -U --noconfirm linux-upstream-api-headers-$kernel_Archlinux-1-x86_64.pkg.tar.zst
+
 else
-  printf "Nope abort!\n\n\n"
-   exit 1
+	echo Something went wrong, manually check.
 fi
+#$NOTIFY "Kernel update process done"
+}
 
-printf "*********Configuring PKGBUILD********** \n\n\n\n"
+which_kernel() {
+printf "\n\n Which kernel would be your base? Stable or Mainline or Longterm? [S/M/L]: %s"
+read response
 
-arch_repo=linux/repos/core-x86_64/
-cd $arch_repo
+if [[ $response == "S" ]];then
+#Get the stable kernel from kernel.org
+kernel=$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 6.13)
+elif [[ $response == "M" ]];then
+#Get the mainline kernel from kernel.org
+kernel=$(curl -s https://www.kernel.org/ | grep -A1 'mainline:' | grep -oP '(?<=strong>).*(?=</strong.*)')
+elif [[ $response == "L" ]];then
+#Get the longterm kernel from kernel.org
+kernel=$(curl -s https://www.kernel.org/ | grep -A1 'longterm:' | grep -oP '(?<=strong>).*(?=</strong.*)')
+fi
+}
 
-sudo zcat /proc/config.gz > config
-
-# Turning off like this help build kernel faster, but pose difficulty while debugging
-
-sed -i "s/"CONFIG_DEBUG_KERNEL=y"/"CONFIG_DEBUG_KERNEL=n"/" config
-grep DEBUG_KERNEL config
-sed -i "s/"CONFIG_DEBUG_INFO=y"/"CONFIG_DEBUG_INFO=n"/" config
-grep DEBUG_INFO config
-
-sed -i "s/pkgbase=linux/pkgbase=$(hostname)-$(echo $kernel) /"  PKGBUILD
-pkgver=$(grep "pkgver" PKGBUILD | head -1)
-sed -i "s/$(echo $pkgver)/pkgver=$(echo $kernel) /" PKGBUILD
-
-sed -i '7d' PKGBUILD
-sed -i '7i _srcver=${pkgver%%%.*}-arch1 '  PKGBUILD
-sed -i '17d' PKGBUILD
-sed -i '17i _srcname=${pkgver%%%.*}-arch1'  PKGBUILD
-
-sed -i '19d' PKGBUILD
-sed -i '19i \"$_srcname::https://github.com/archlinux/linux/archive/refs/tags/v$_srcname.tar.gz\"' PKGBUILD
-sed -i '31d' PKGBUILD
-sed -i '31i export KBUILD_BUILD_HOST=Bhaskar_ThinkPad_x250' PKGBUILD
-sed -i '32d' PKGBUILD
-sed -i '32i export KBUILD_BUILD_USER=Bhaskar' PKGBUILD
-sed -i '36d' PKGBUILD
-sed -i '36i cd src/linux-$_srcname' PKGBUILD
-sed -i 's/#make oldconfig/make olddefconfig/' PKGBUILD
-sed -i '62d' PKGBUILD
-sed -i '62i make V=1 ARCH=x86_64 -j4' PKGBUILD
-sed -i '63d' PKGBUILD
-sed -i '174,192 s/^/#/' PKGBUILD
-sed -i '193d' PKGBUILD
-sed -i '193i pkgname=("$pkgbase" "$pkgbase-headers")' PKGBUILD
-
-printf "As we have change the PKGBUILD file ,we need to generate the new CHECKSUM the file ....  \n\n\n"
-
-#makepkg -g
-
-updpkgsums
-
-
-printf "\n\n\n Lets do the compiling now  ....\n\n\n"
-
-$TM "\t\n\n Elapsed Time : %E \n\n"  makepkg -s
-
-
-printf "Install the generated headers,kernel and doc packages with pacman .. \n\n\n"
-
-
-cd $KERNEL_PKG_DIR
-
-sudo pacman -U  --noconfirm $(hostname)-$kernel-$kernel-1-x86_64.pkg.tar.zst
-
-sudo pacman -U --noconfirm $(hostname)-$kernel-headers-$kernel-1-x86_64.pkg.tar.zst
-
-
-# printf "\n\n\n Done..now copy over the image to ${Yellow}EFI dir..${NOCOLOR} \n\n\n\n"
-
-# sudo cp -v /boot/vmlinuz-$(hostname)-$kernel $EFIBOOTDIR/$(hostname)/
-# sudo cp -v /boot/initramfs-$(hostname)-$kernel.img $EFIBOOTDIR/$(hostname)/
-
-
-# printf "Fixed the boot entry now ...\n\n\n\n"
-
-# echo "title ArchLinux" | sudo tee  $EFIBOOTENTRY/ArchLinux.conf
-# echo "linux /EFI/ArchLinux/vmlinuz-$(hostname)-$kernel" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
-# echo "initrd /EFI/ArchLinux/initramfs-$(hostname)-$kernel.img" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
-# echo "options root=PARTUUID=9e3d2f9a-4846-3049-97fc-b5e5c61820ae  loglevel=3  systemd.show_status=true rw" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
-
-# printf "\n\n\n Modified the UEFI script...  \n\n"
-
-# echo "\EFI\ArchLinux\vmlinuz-$(hostname)-$kernel root=PARTUUID=9e3d2f9a-4846-3049-97fc-b5e5c61820ae  loglevel=3  systemd.show_status=true rw initrd=\EFI\ArchLinux\initramfs-$(hostname)-$kernel.img" | sudo tee  /boot/efi/EFI/archlinux.nsh
-exit 0
+archlinux_kernel_build
