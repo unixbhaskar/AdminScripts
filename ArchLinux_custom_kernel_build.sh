3efeb141e66d4e7f30f777537f23e8d1c1d92f07
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2025-03-11 03:49:12 +0530

    Arch Linux custom kernel build from upstream Linux kernel source
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- ArchLinux_custom_kernel_build.sh
+++ ArchLinux_custom_kernel_build.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+
+# This script is to build custom arch linux kernel from upstream linux kernel
+# source
+
+Author: Bhaskar Chowdhury
+Date: Tue 11 Mar 2025 03:47:05 AM IST
+Email: unixbhaskar@gmail.com
+
+# License (GPL v2.0)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
 get_make=$(command -v make)
 get_elapsed_time="/usr/bin/time -f"
 untar_it="tar -xJvf"
