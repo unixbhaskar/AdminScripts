d197503f4fd2a088ececd9f7c2acf562cc7eb2fe
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2026-05-27 11:02:26 +0530

    Add config flags for performance imporvement
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- emacs_build_from_source.sh
+++ emacs_build_from_source.sh
@@ -23,13 +23,18 @@ basepath=/home/bhaskar/git-linux/
 cd $basepath
 
 if [ ! -d emacs ];then
-	git clone git@git://git.savannah.gnu.org/emacs.git
+	git clone git://git.savannah.gnu.org/emacs.git
 	cd emacs
 
 elif [ -d emacs ];then
        cd emacs
-       git pull
+# discard stuff from last build
+      git reset --hard
 
+# delete all of the last build stuff
+      git clean -xdf
+
+       git pull
 else
 	printf "The Emacs directory already exists....not cloning \n"
 	exit 1
@@ -37,10 +42,13 @@ fi
 
 printf "Lets build it on.....$(hostname)"
 
+./autogen.sh
+# ./configure --with-pgtk
+# ./configure --with-gnutls=ifavailable --with-native-compilation --with-libxml2
 
-./configure
+./configure --with-x --with-x-toolkit=gtk3 --without-toolkit-scroll-bars --with-cairo --without-xft --with-harfbuzz --without-libotf --with-gnutls --without-xdbe --without-xim --without-gpm --disable-gc-mark-trace --with-gsettings --with-modules --with-threads --with-libgmp --with-xml2 --with-tree-sitter --with-zlib --without-included-regex --with-native-compilation --with-file-notification=inotify --without-compress-install
 
-make
+make -j "$(nproc)" -l "$(nproc --ignore=1)"
 
 
 if [ $? -eq 0 ] ; then
@@ -49,7 +57,11 @@ if [ $? -eq 0 ] ; then
 	read -r res
 
 	if [ "$res" == "Y" ];then
-		sudo make install
+		sudo mv /usr/bin/emacs{,.$(date +'%F%T')}
+		sudo make install-strip
+		sudo cp /usr/local/bin/emacs /usr/bin/
+
+
 	else
 		printf "Okay, you not want to install it system wide....not installing "
 fi
